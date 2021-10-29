c3506adca3a72ee9ee2353497b281c4d0b067185
Ohzzi <clover7even@naver.com>
2021-10-29 13:10:12 +0900

    Init: 백엔드 프로젝트 생성

---
+++ backend/src/main/java/com/back/websters/WebstersApplication.java
@@ -0,0 +1,13 @@
+package com.back.websters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstersApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/back/websters/WebstersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.websters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
