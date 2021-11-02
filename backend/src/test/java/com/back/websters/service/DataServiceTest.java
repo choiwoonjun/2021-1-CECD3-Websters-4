2d36d48c2c4121e82c68b2f476770cc851ba29cb
Oh <clover7even@naver.com>
2021-11-02 21:37:52 +0900

    Test: 스크립트 저장 테스트

--- backend/src/test/java/com/back/websters/service/DataServiceTest.java
+++ backend/src/test/java/com/back/websters/service/DataServiceTest.java
@@ -2,6 +2,8 @@ package com.back.websters.service;
 
 import com.back.websters.domain.bookmark.Bookmark;
 import com.back.websters.domain.bookmark.BookmarkRepository;
+import com.back.websters.domain.script.Script;
+import com.back.websters.domain.script.ScriptRepository;
 import com.back.websters.domain.video.Video;
 import com.back.websters.domain.video.VideoRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -19,11 +21,14 @@ class DataServiceTest {
     @Autowired
     private BookmarkRepository bookmarkRepository;
     @Autowired
+    private ScriptRepository scriptRepository;
+    @Autowired
     private DataService dataService;
 
     @AfterEach
     void afterEach() {
         bookmarkRepository.deleteAll();
+        scriptRepository.deleteAll();
         videoRepository.deleteAll();
     }
 
@@ -34,10 +39,10 @@ class DataServiceTest {
         String videoUri = "S3://test/myvideo.mp4";
         long id = dataService.saveVideo(videoName, videoUri);
 
-        // then
+        // when
         Video result = videoRepository.findById(id).get();
 
-        // when
+        // then
         assertThat(result.getName()).isEqualTo(videoName);
         assertThat(result.getUri()).isEqualTo(videoUri);
 
@@ -50,10 +55,10 @@ class DataServiceTest {
         String videoUri = "S3://test/myvideo.mp4";
         long id = dataService.saveVideo(videoName, videoUri);
 
-        // then
+        // when
         Video result = dataService.findVideoByName(videoName);
 
-        // when
+        // then
         assertThat(result.getId()).isEqualTo(id);
         assertThat(result.getUri()).isEqualTo(videoUri);
 
@@ -69,12 +74,39 @@ class DataServiceTest {
         String time = "00:55";
         long bookmarkId = dataService.saveBookmark(videoRepository.findById(videoId).get().getId(), time);
 
-        // then
+        // when
         Bookmark result = bookmarkRepository.findById(bookmarkId).get();
 
-        //when
+        // then
         assertThat(result.getId()).isEqualTo(bookmarkId);
         assertThat(result.getTime()).isEqualTo(time);
+
+    }
+
+    @Test
+    void 스크립트_저장() {
+        // given
+        String videoName = "myvideo.mp4";
+        String videoUri = "S3://test/myvideo.mp4";
+        long videoId = dataService.saveVideo(videoName, videoUri);
+
+        String startTime = "00:00";
+        String endTime = "00:03";
+        String content = "테스트";
+        Script script = Script.builder()
+                .startTime(startTime)
+                .endTime(endTime)
+                .content(content).build();
+        long scriptId = dataService.saveScript(videoId, script);
+
+        // when
+        Script result = scriptRepository.findById(scriptId).get();
+
+
+        // then
+        assertThat(result.getContent()).isEqualTo(content);
+        assertThat(result.getStartTime()).isEqualTo(startTime);
+        assertThat(result.getEndTime()).isEqualTo(endTime);
 
     }
 
