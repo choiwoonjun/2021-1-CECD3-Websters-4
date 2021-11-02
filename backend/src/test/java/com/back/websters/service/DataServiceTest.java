0e9d7193bacb4afc5f900feb840380e2d5b81c96
Oh <clover7even@naver.com>
2021-11-02 21:29:57 +0900

    Test: 북마크 저장 기능 테스트

--- backend/src/test/java/com/back/websters/service/DataServiceTest.java
+++ backend/src/test/java/com/back/websters/service/DataServiceTest.java
@@ -1,6 +1,7 @@
 package com.back.websters.service;
 
 import com.back.websters.domain.bookmark.Bookmark;
+import com.back.websters.domain.bookmark.BookmarkRepository;
 import com.back.websters.domain.video.Video;
 import com.back.websters.domain.video.VideoRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -16,10 +17,13 @@ class DataServiceTest {
     @Autowired
     private VideoRepository videoRepository;
     @Autowired
+    private BookmarkRepository bookmarkRepository;
+    @Autowired
     private DataService dataService;
 
     @AfterEach
     void afterEach() {
+        bookmarkRepository.deleteAll();
         videoRepository.deleteAll();
     }
 
@@ -52,6 +56,25 @@ class DataServiceTest {
         // when
         assertThat(result.getId()).isEqualTo(id);
         assertThat(result.getUri()).isEqualTo(videoUri);
+
+    }
+
+    @Test
+    void 북마크_저장() {
+        // given
+        String videoName = "myvideo.mp4";
+        String videoUri = "S3://test/myvideo.mp4";
+        long videoId = dataService.saveVideo(videoName, videoUri);
+
+        String time = "00:55";
+        long bookmarkId = dataService.saveBookmark(videoRepository.findById(videoId).get().getId(), time);
+
+        // then
+        Bookmark result = bookmarkRepository.findById(bookmarkId).get();
+
+        //when
+        assertThat(result.getId()).isEqualTo(bookmarkId);
+        assertThat(result.getTime()).isEqualTo(time);
 
     }
 
