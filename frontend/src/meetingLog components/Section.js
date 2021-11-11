bb721739b89234105d98f631a085ed858491169c
nubee <nnubiapascal@gmail.com>
2021-11-11 19:11:22 +0900

    modified the menu and icons

--- frontend/src/meetingLog components/Section.js
+++ frontend/src/meetingLog components/Section.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import time from 'assets/img/logo/time.png';
 import './style.scss';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 // import { Carousel } from 'antd';
-import { Carousel } from '@trendyol-js/react-carousel';
+// import { Carousel } from '@trendyol-js/react-carousel';
+// var Carousel = require('react-responsive-carousel').Carousel;
+// import Carousel from "@davistran86/carousel";
+// import {
+//   MDBCarousel,
+//   MDBCarouselInner,
+//   MDBCarouselItem,
+//   MDBCarouselElement,
+// } from 'mdb-react-ui-kit';
 function Section(props) {
   const [Info, setInfo] = useState({});
   useEffect(() => {
@@ -18,74 +28,34 @@ function Section(props) {
     textAlign: 'center',
     background: '#364d79',
     margin: '1.5rem auto',
-    paddingLeft: '10px'
+    paddingLeft: '10px',
   };
   return (
     
-    
     <div className="Meeting_container">
-            {/* <Carousel afterChange={""}>
-    <div className="carousel">
-      <h3 style={contentStyle}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-  </Carousel> */}
-
-    {/* <h1 color="#2d66c3">We love Web1 🌐</h1>
-    <h1 color="#2d66c3">We love Web2 🌐</h1>
-    <h1 color="#2d66c3">We love Web3 🌐</h1>
-    <h1 color="#2d66c3">We love Web4 🌐</h1> */}'
-  {/* <button>Click</button>
-    <div>
-      <h1>{Info.transcripts &&
-          Info.transcripts.map(t => {
-            return (              
-              <div
-                className="speech"
-                onClick={() => {
-                  video.currentTime = t.time;
-                  video.play();
-                }}
-              >
-                <img src={time} style={{ width: '20px' }} />
-                {t.time} : {t.speech}
-              </div>
-            );
-          })}</h1>
-    
-          </div> */}
-
-
-<Carousel show={1} slide={1} swiping={true} >
-      <div className="">
+      <div className="smallerVideo">
         <img className="video" src={imageUrl} />
       </div>
-
       <div className="meeting_log">
         {Info.transcripts &&
           Info.transcripts.map(t => {
-            return (              
-              <div
-                className="speech"
-                onClick={() => {
-                  video.currentTime = t.time;
-                  video.play();
-                }}
-              >
-                {/* <img src={time} style={{ width: '20px' }} /> */}
-                {t.time} : {t.speech}
+            return (
+              <div>
+                <div
+                  className="speech"
+                  onClick={() => {
+                    video.currentTime = t.time;
+                    video.play();
+                  }}
+                >
+                  <img src={time} style={{ width: '20px' }} />
+                  {t.time} : {t.speech}
+
+                </div>
               </div>
             );
           })}
-      </div></Carousel>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -1,91 +1,113 @@
-import * as React from 'react';
+import  React, { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 // import { Editor, EditorTools } from "@progress/kendo-react-editor";
 // import content from "./content-overview";
 import { RichTextEditor } from 'react-rte';
 import { CKEditor } from 'ckeditor4-react';
-// const {
-//   Bold,
-//   Italic,
-//   Underline,
-//   Strikethrough,
-//   Subscript,
-//   Superscript,
-//   AlignLeft,
-//   AlignCenter,
-//   AlignRight,
-//   AlignJustify,
-//   Indent,
-//   Outdent,
-//   OrderedList,
-//   UnorderedList,
-//   Undo,
-//   Redo,
-//   FontSize,
-//   FontName,
-//   FormatBlock,
-//   Link,
-//   Unlink,
-//   InsertImage,
-//   ViewHtml,
-//   InsertTable,
-//   AddRowBefore,
-//   AddRowAfter,
-//   AddColumnBefore,
-//   AddColumnAfter,
-//   DeleteRow,
-//   DeleteColumn,
-//   DeleteTable,
-//   MergeCells,
-//   SplitCell,
-// } = EditorTools;
-{
-  /* <CKEditor data="<p>Editor's content</p>"/> */
-}
+import { Table, Modal, Button } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import { UploadOutlined, ExportOutlined, SettingOutlined } from '@ant-design/icons';
+const { SubMenu } = Menu;
+const columns = [
+	{
+	  title: 'Import',
+	  dataIndex: 'video_name',
+	  render: text => <a>{text}</a>,
+	  align:'center',
+	  columnWidth:100, 
+	  width:100
+	  
+	},
+	{
+	  title: 'Export',
+	  className: 'column-money',
+	  dataIndex: 'video_id',
+	  align: 'center',
+	  width:100,
+	  render:(id)=><a href={`/video/Editor/${id}`}>Export <Button type="primary" shape="round" icon={<DownloadOutlined />} size={""} /></a>
+	},
+	{
+	  title: 'Settings',
+	  dataIndex: 'video_id',
+	  align:'center',
+	  render:(id)=><a href={`/video/${id}`}>Settings</a>,
+	  width:100
+	},
+  ];
 
-{
-  /* <Editor
-      tools={[
-        [Bold, Italic, Underline, Strikethrough],
-        [Subscript, Superscript],
-        [AlignLeft, AlignCenter, AlignRight, AlignJustify],
-        [Indent, Outdent],
-        [OrderedList, UnorderedList],
-        FontSize,
-        FontName,
-        FormatBlock,
-        [Undo, Redo],
-        [Link, Unlink, InsertImage, ViewHtml],
-        [InsertTable],
-        [AddRowBefore, AddRowAfter, AddColumnBefore, AddColumnAfter],
-        [DeleteRow, DeleteColumn, DeleteTable],
-        [MergeCells, SplitCell],
-      ]}
-      contentStyle={{
-        height: 630,
-      }}
-      defaultContent={content}
-    /> */
-}
 
 const EditorPage = () => {
+	const [visible, setVisible] = useState(false);
+	const [current,setCurrent] = useState(false);
+	
+	function handleClick(){
+		// console.log('click ', e);
+		setCurrent(current)
+	  };
   return (
     <div>
       <div className="editor-page">
         <h3>Editor Page</h3>
       
-      <div class="navbarr">
-        <a href="#home">Import</a>
-        <a href="#news">Edit</a>
-        <a href="#home">Setting</a>
-      </div>
+      {/* <div class="navbarr">
+        <a>Import</a>
+        <a>Export</a>
+        <a>Setting</a>
+      </div> */}
       {/* <div>
         <RichTextEditor date="data" />
       </div> */}
+	   <div>
+		   {/* <columns></columns>
+            <Table
+            columns={columns}
+        /> */}
+		<div>
+		<Menu onClick={""} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="mail" icon={<UploadOutlined />}>
+          Import
+        </Menu.Item>
+        <Menu.Item key="app" icon={<ExportOutlined />}>
+          Export
+        </Menu.Item>
+        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Settings">
+          <Menu.ItemGroup title="Item 1">
+            {/* <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item> */}
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Item 2">
+            {/* <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item> */}
+          </Menu.ItemGroup>
+        </SubMenu>
+        <Menu.Item key="alipay">
+          {/* <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+            Navigation Four - Link
+          </a> */}
+        </Menu.Item>
+      </Menu>
+		</div>
+        </div>
       <CKEditor data="<p>Hello from the first editor working with the context!</p>"></CKEditor>
       <div className="button-center">
-      <button className="button">Save</button>
-      <button>Reset</button>
+      {/* <button className="button">Save</button> */}
+	  <Button type="primary" onClick={() => setVisible(true)}>
+       저장
+      </Button>
+	  <Modal
+        title="수정한 회의록을 저장하시겠습니까?"
+        centered
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        width={500}
+      >
+      </Modal>
+      {/* <button>Reset</button> */}
+	  <Button danger onClick={() => ""}>
+       취소
+      </Button>
       </div>
       </div>
     </div>
